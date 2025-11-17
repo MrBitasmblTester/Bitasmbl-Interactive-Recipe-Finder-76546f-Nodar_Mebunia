b14e1b424cb3428cbdb757b73e270caccbd2a4c8
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-11-17 23:01:19 +0400

    Add SearchBar.vue

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <input class="search" v-model="query" @input="$emit('search',query)" placeholder="Search recipes or ingredients" />
+</template>
+<script setup>
+import { ref } from "vue"
+const emits = defineEmits(["search"])
+const query = ref("")
+</script>
