22a55a71def685e8864fc4848e555f0c51de1978
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-11-17 23:01:24 +0400

    Add RecipeDetail.vue

---
+++ src/views/RecipeDetail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="detail container">
+    <button @click="$router.back()">Back</button>
+    <h2>{{ recipe.title }}</h2>
+    <ul><li v-for="(s,i) in recipe.steps" :key="i">{{ s }}</li></ul>
+  </div>
+</template>
+<script setup>
+import { ref, onMounted } from "vue"
+import { fetchRecipeById } from "../services/api"
+const props = defineProps(["id"])
+const recipe = ref({ title: "", steps: [] })
+onMounted(async ()=> recipe.value = await fetchRecipeById(props.id))
+</script>
