7a2346a007d038d143ca1204d9b57525cf357893
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-11-17 23:01:20 +0400

    Add RecipeCard.vue

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="card">
+    <h3>{{ recipe.title }}</h3>
+    <p v-if="recipe.summary">{{ recipe.summary }}</p>
+    <button @click="$emit('view',recipe.id)">View</button>
+  </div>
+</template>
+<script setup>
+defineProps({ recipe: Object })
+</script>
